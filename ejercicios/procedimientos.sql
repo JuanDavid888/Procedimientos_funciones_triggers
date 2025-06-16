b7503166bbc2843ed1f80aea16f06d09a408a83c
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-15 19:01:20 -0500

    feat: :sparkles: Se realizó el segundo ejercicio de procedimientos

--- ejercicios/procedimientos.sql
+++ ejercicios/procedimientos.sql
@@ -88,3 +88,58 @@ JOIN ingrediente_producto ing_pro ON ing_pro.producto_id = pro.id
 JOIN ingrediente ing ON ing.id = ing_pro.ingrediente_id
 WHERE pro.id = 2
 GROUP BY pro.id, pro.nombre, pro_pre.precio;
+
+-- 2
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ps_actualizar_precio_pizza $$
+
+CREATE PROCEDURE ps_actualizar_precio_pizza(
+    IN p_pizza_id INT,
+    IN p_presentacion_id INT,
+    IN p_nuevo_precio DECIMAL(10, 2)
+)
+BEGIN
+
+    -- Validar que la pizza existe
+    IF NOT EXISTS (SELECT 1 FROM producto WHERE id = p_pizza_id) THEN
+        SIGNAL SQLSTATE '40002' 
+            SET MESSAGE_TEXT = 'La pizza seleccionada no existe.'; -- Verfica que exista en alguna fila, si no, lanza error
+    END IF;
+
+    -- Validar que la presentacion existe
+    IF NOT EXISTS (SELECT 1 FROM presentacion WHERE id = p_presentacion_id) THEN
+        SIGNAL SQLSTATE '40002' 
+            SET MESSAGE_TEXT = 'La presentacion seleccionada no existe.'; -- Verfica que exista en alguna fila, si no, lanza error
+    END IF;
+
+    IF p_nuevo_precio <= 0 THEN
+        SIGNAL SQLSTATE '40001' 
+            SET MESSAGE_TEXT = 'El precio debe ser mayor a 0.'; -- Verfica que el precio sea mayor a 0, si no, lanza error
+    END IF;
+
+    -- Actualizar precio
+    UPDATE producto_presentacion SET precio = p_nuevo_precio
+    WHERE producto_id = p_pizza_id AND
+    presentacion_id = p_presentacion_id;
+
+    -- Mostrar resultados
+    SELECT 
+    pro.nombre AS Producto,
+    pro_pre.precio AS Precio
+    FROM producto pro
+    JOIN producto_presentacion pro_pre ON pro_pre.producto_id = pro.id
+    WHERE pro.id = p_pizza_id AND pro_pre.presentacion_id = p_presentacion_id;
+
+END$$
+
+DELIMITER ;
+
+CALL ps_actualizar_precio_pizza(1, 3, 14000);
+
+SELECT 
+    pro.nombre AS Producto,
+    pro_pre.precio AS Precio
+FROM producto pro
+JOIN producto_presentacion pro_pre ON pro_pre.producto_id = pro.id
+WHERE pro.id = 1 AND pro_pre.presentacion_id = 3;
